8eda3a6b5eb27b1f9cbb63c457000630691b2775
Slindile <slindilenkabs23@gmail.com>
2026-01-24 21:14:12 +0200

    connected to the api to get data using a get request for my extract function

---
+++ ETL_Pipeline/etl_pipeline.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as pd
+
+def extract(): 
+
+    FAKE_STORE_URL = "https://fakestoreapi.com/products" # url for all products 
+
+    try:
+        response = requests.get(FAKE_STORE_URL)  #response from api
+        response.raise_for_status()  # automatically raises an exception when an error occurs
+
+        data = response.json() #returns the data as json
+        df = pd.DataFrame(data)
+        print(df) # prints data in the form of a dataFrame .
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred : {e}")
+
+if __name__ == "__main__":
+    extract()
