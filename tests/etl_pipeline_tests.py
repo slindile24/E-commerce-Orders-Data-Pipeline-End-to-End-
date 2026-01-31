ff1beab2dca30eddd1b7e3c556e329fd734cd236
= <slindilenkabs23@gmail.com>
2026-01-31 20:13:35 +0200

    testing for correct datatypes

--- tests/etl_pipeline_tests.py
+++ tests/etl_pipeline_tests.py
@@ -1,6 +1,6 @@
 import unittest
 import pandas as pd
-from ETL_Pipeline.etl_pipeline import extract
+from ETL_Pipeline.etl_pipeline import *
 
 from unittest.mock import patch
 
@@ -21,6 +21,38 @@ class TestETL(unittest.TestCase):
         self.assertFalse(df.empty) # testing if we got data back 
         self.assertIn('rate',df.columns) # testing if rate is in columns
         self.assertIn('count',df.columns) # testing if count is in columns
+
+    
+    def test_correct_datatypes(self):
+
+        raw_df = pd.DataFrame({
+        "id": ["1", "2"],  # inserted strings on purpose
+        "title": ["Bag", "Shirt"],
+        "price": ["109.95", "29.99"], 
+        "rating": [
+            {"rate": 3.9, "count": 120},
+            {"rate": 4.5, "count": 10}
+        ]
+        })
+
+        transformed_df = transform(raw_df)
+        print(transformed_df.dtypes)
+
+
+        self.assertTrue(pd.api.types.is_integer_dtype(transformed_df["id"]))
+        self.assertTrue(pd.api.types.is_float_dtype(transformed_df["price"]))
+        self.assertTrue(pd.api.types.is_float_dtype(transformed_df["rate"]))
+        self.assertTrue(pd.api.types.is_integer_dtype(transformed_df["count"]))
+        self.assertTrue(pd.api.types.is_string_dtype(transformed_df["title"]))
+  
+
+
+
+
+
+
+   
+        
         
 
 if __name__ == "__main__":
